034dc7048ece00deb7aed8273838578e17feb224
denis96z <zinoviev.denis@yandex.ru>
2018-04-25 15:42:48 -0700

    Hidden layers sizes set to [512,256,64].

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -21,31 +21,13 @@ TEST_SETS_DIR = './data/test_sets'
 def main():
     p = Perceptron()
 
-    p.set_learning_rate(0.1)
+    p.set_learning_rate(0.5)
     p.set_act_func(sigmoid, sigmoid_dif)
 
-    p.add_layer(num_inputs=NUM_INPUTS, num_outputs=20, threshold=0.5)
-    p.add_layer(num_outputs=NUM_OUTPUTS, threshold=0.1)
-
-    print('LEARNING...')
-    for i in range(NUM_EPOCHS):
-        for tr_set in range(NUM_TRAINING_SETS):
-            for letter in LETTERS:
-                path = make_file_path(TRAINING_SETS_DIR, tr_set, letter)
-                in_vector = load_image_from_file(path)
-                expected_out_vector = make_expected_output_vector(letter)
-                p.learn(in_vector, expected_out_vector)
-                print('LEARNED "' + letter + '" FROM SET #' + str(tr_set))
-        print('EPOCH #' + str(i) + ' passed')
-
-    print('TESTING...')
-    for ts_set in range(NUM_TEST_SETS):
-        for letter in LETTERS:
-            path = make_file_path(TEST_SETS_DIR, ts_set, letter)
-            in_vector = load_image_from_file(path)
-            predicted_index = p.predict(in_vector)
-            print('SET: #' + str(ts_set) + '; EXPECTED: "' + letter +
-                  '"; PREDICTED: "' + LETTERS[predicted_index] + '"')
+    p.add_layer(num_inputs=NUM_INPUTS, num_outputs=512, threshold=0.5)
+    p.add_layer(num_outputs=256, threshold=0.2)
+    p.add_layer(num_outputs=64, threshold=0.1)
+    p.add_layer(num_outputs=NUM_OUTPUTS, threshold=0.05)
 
 
 def sigmoid(x):
@@ -57,13 +39,13 @@ def sigmoid_dif(x):
     return temp / ((1 + temp) ** 2)
 
 
-def make_file_path(root, set, letter):
-    return root + '/' + str(set) + '/' + letter + '.png'
+def make_file_path(root, set_num, letter):
+    return root + '/' + str(set_num) + '/' + letter + '.png'
 
 
 def make_expected_output_vector(letter):
     index = ord(letter) - ord('A')
-    vector = [0 for i in range(len(LETTERS))]
+    vector = [0 for i in LETTERS]
     vector[index] = 1
     return vector
 
@@ -72,6 +54,17 @@ def load_image_from_file(path):
     img = Image.open(path).resize(IMG_SIZE)
     bts = [(0 if b == 0 else 1) for b in img.tobytes()]
     return bts
+
+
+def load_sets(root_dir, num_sets):
+    x, y = [], []
+    for cur_set in range(num_sets):
+        for letter in LETTERS:
+            path = make_file_path(root_dir, cur_set, letter)
+            cur_x = load_image_from_file(path)
+            cur_y = make_expected_output_vector(letter)
+            x.append(cur_x), y.append(cur_y)
+    return x, y
 
 
 if __name__ == "__main__":
