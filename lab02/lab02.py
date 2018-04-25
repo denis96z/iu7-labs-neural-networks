656380c2036147a696f6385a2bae41b364b3fe74
denis96z <zinoviev.denis@yandex.ru>
2018-04-25 05:30:05 -0700

    Image format fixed in lab02. Accuracy is still low.

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -4,51 +4,54 @@ from keras import Sequential, losses
 from keras.layers import Dense
 from keras.optimizers import SGD
 
-IMG_SIZE = (5, 8)
+IMG_SIZE = (10, 10)
 LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
-NUM_INPUTS = IMG_SIZE[0] * IMG_SIZE[1]
+NUM_INPUTS = IMG_SIZE[0] * IMG_SIZE[1] * 4
 NUM_OUTPUTS = len(LETTERS)
 
-
-NUM_TRAINING_SETS = 4
-NUM_REPEATS = 1000
+NUM_TRAINING_SETS = 100
+NUM_EPOCHS = 100
+VALIDATION_SPLIT = 0.2
 TRAINING_SETS_DIR = './../lab01/data/training_sets'
 
-NUM_TEST_SETS = 1
+NUM_TEST_SETS = 50
 TEST_SETS_DIR = './../lab01/data/test_sets'
 
 
 def main():
-    x_train, y_train = load_sets(TRAINING_SETS_DIR, NUM_TRAINING_SETS)
-    x_train = x_train.astype('float32') / 255
-    y_train = y_train.astype('float32') / 255
+    try:
+        x_train, y_train = load_sets(TRAINING_SETS_DIR, NUM_TRAINING_SETS)
+        x_train = x_train.astype('float32') / 255
+        y_train = y_train.astype('float32') / 255
 
-    x_test, y_test = load_sets(TEST_SETS_DIR, NUM_TEST_SETS)
-    x_test = x_test.astype('float32') / 255
-    y_test = y_test.astype('float32') / 255
+        x_test, y_test = load_sets(TEST_SETS_DIR, NUM_TEST_SETS)
+        x_test = x_test.astype('float32') / 255
+        y_test = y_test.astype('float32') / 255
 
-    model = Sequential()
-    model.add(Dense(NUM_OUTPUTS, input_shape=(NUM_INPUTS,),
-                    kernel_initializer='random_normal',
-                    use_bias=True,
-                    bias_initializer='random_uniform',
-                    activation='sigmoid'))
-    model.summary()
-    model.compile(loss=losses.mean_squared_error,
-                  optimizer=SGD(), metrics=['accuracy'])
+        model = Sequential()
+        model.add(Dense(NUM_OUTPUTS, input_shape=(NUM_INPUTS,),
+                        kernel_initializer='random_normal',
+                        use_bias=True,
+                        bias_initializer='random_uniform',
+                        activation='sigmoid'))
+        model.summary()
+        model.compile(loss=losses.mean_squared_error,
+                      optimizer=SGD(), metrics=['accuracy'])
 
-    history = model.fit(x_train, y_train,
-                        batch_size=1, epochs=1000,
-                        verbose=1, validation_split=0)
-    score = model.evaluate(x_test, y_test, verbose=1)
+        history = model.fit(x_train, y_train,
+                            batch_size=1, epochs=NUM_EPOCHS,
+                            verbose=1, validation_split=VALIDATION_SPLIT)
+        score = model.evaluate(x_test, y_test, verbose=1)
 
-    print('Test score: ', score[0])
-    print('Test accuracy: ', score[1])
+        print('Test score: ', score[0])
+        print('Test accuracy: ', score[1])
+    except:
+        print('Error occurred...')
 
 
-def make_file_path(root, set, letter):
-    return root + '/' + str(set) + '/' + letter + '.bmp'
+def make_file_path(root, set_num, letter):
+    return root + '/' + str(set_num) + '/' + letter + '.png'
 
 
 def make_expected_output_vector(letter):
@@ -59,16 +62,14 @@ def make_expected_output_vector(letter):
 
 
 def load_image_from_file(path):
-    try:
-        img = Image.open(path).resize(IMG_SIZE)
-        return [0 if px == 255 else 1 for px in img.tobytes()]
-    except IOError:
-        return None
+    img = Image.open(path).resize(IMG_SIZE)
+    bts = [(0 if b == 0 else 1) for b in img.tobytes()]
+    return bts
 
 
 def load_sets(root_dir, num_sets):
     x, y = [], []
-    for cur_set in range(1, num_sets + 1):
+    for cur_set in range(num_sets):
         for letter in LETTERS:
             path = make_file_path(root_dir, cur_set, letter)
             cur_x = load_image_from_file(path)
